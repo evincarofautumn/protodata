0ece1938f2b002db9b07093c3e5a155d64d04aa3
Jon Purdy <evincarofautumn@gmail.com>
2012-11-12 09:37:09 -0800

    Fix premature termination of token stream.

--- tokenizer.h
+++ tokenizer.h
@@ -2,6 +2,7 @@
 #define PROTODATA_TOKENIZER_H
 
 #include <iterator>
+#include <deque>
 #include <stdexcept>
 
 #include <rx/ignorer.h>
@@ -14,14 +15,11 @@
 template<class R>
 class tokenizer_t {
 public:
-  tokenizer_t(tokenizer_t&& other)
-    : input(std::move(other.input)), current(other.current) {}
+  tokenizer_t(tokenizer_t&&) = default;
   tokenizer_t(R&& range) : input(std::move(range)) { advance(); }
-  bool empty() const { return input.empty(); }
-  void pop_front() { advance(); }
-  Token front() const {
-    return current;
-  }
+  bool empty() const { return input.empty() && buffer.empty(); }
+  void pop_front() { buffer.pop_front(); advance(); }
+  Token front() const { return buffer.front(); }
   tokenizer_t() = delete;
   tokenizer_t(const tokenizer_t&) = delete;
   tokenizer_t& operator=(tokenizer_t&&) = delete;
@@ -34,21 +32,20 @@ private:
     ignorer ignore;
     auto output(utf8_output_encoder(output_iterator(back_inserter(token))));
     while (accept_if(is_whitespace, input, ignore)) {}
+    if (input.empty()) return;
     if (accept(0x7b, input, ignore)) {
-      current = Token::push();
+      buffer.push_back(Token::push());
     } else if (accept(0x7d, input, ignore)) {
-      current = Token::pop();
+      buffer.push_back(Token::pop());
     } else if (accept_if(is_alphabetic, input, output)) {
       while (accept_if(is_alphanumeric, input, output)) {}
       throw runtime_error(join("Unknown keyword: \"", token, "\"."));
     } else if (!input.empty()) {
       throw runtime_error(join("Invalid character '", input.front(), "'."));
-    } else {
-      throw runtime_error("Unexpected end of input.");
     }
   }
   R input;
-  Token current;
+  std::deque<Token> buffer;
 };
 
 template<class R>
