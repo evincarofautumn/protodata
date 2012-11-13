0cd7a420c7bf6ac898022d7d2231079bf83abeeb
Jon Purdy <evincarofautumn@gmail.com>
2012-11-13 10:11:18 -0800

    Sort chartype functions.

--- chartype.h
+++ chartype.h
@@ -1,17 +1,27 @@
 #ifndef PROTODATA_CHARTYPE_H
 #define PROTODATA_CHARTYPE_H
 
+typedef bool chartype_function(uint32_t);
+chartype_function
+  is_alphabetic,
+  is_alphanumeric,
+  is_binary,
+  is_decimal,
+  is_hexadecimal,
+  is_octal,
+  is_whitespace;
+
 inline bool is_alphabetic(uint32_t rune) {
   return (rune >= U'A' && rune <= U'Z')
     || (rune >= U'a' && rune <= U'z');
 }
 
-inline bool is_binary(uint32_t rune) {
-  return rune == U'0' || rune == U'1';
+inline bool is_alphanumeric(uint32_t rune) {
+  return is_alphabetic(rune) || is_decimal(rune);
 }
 
-inline bool is_octal(uint32_t rune) {
-  return rune >= U'0' && rune <= U'7';
+inline bool is_binary(uint32_t rune) {
+  return rune == U'0' || rune == U'1';
 }
 
 inline bool is_decimal(uint32_t rune) {
@@ -24,8 +34,8 @@ inline bool is_hexadecimal(uint32_t rune) {
     || (rune >= U'a' && rune <= U'f');
 }
 
-inline bool is_alphanumeric(uint32_t rune) {
-  return is_alphabetic(rune) || is_decimal(rune);
+inline bool is_octal(uint32_t rune) {
+  return rune >= U'0' && rune <= U'7';
 }
 
 inline bool is_whitespace(uint32_t rune) {
