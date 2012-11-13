aa80c9af0f1f19e3d1784100418fcb5f97315803
Jon Purdy <evincarofautumn@gmail.com>
2012-11-13 00:22:04 -0800

    Make tokenizer more legible.

--- tokenizer.h
+++ tokenizer.h
@@ -15,67 +15,97 @@
 template<class R>
 class tokenizer_t {
 public:
+
   tokenizer_t(tokenizer_t&&) = default;
-  tokenizer_t(R&& range) : input(std::move(range)) { advance(); }
-  bool empty() const { return input.empty() && buffer.empty(); }
-  void pop_front() { buffer.pop_front(); advance(); }
+
+  tokenizer_t(R&& range) : input(std::move(range)),
+    output(rx::output_iterator(std::back_inserter(buffer))) {
+    advance(output);
+  }
+
+  bool empty() const {
+    return input.empty() && buffer.empty();
+  }
+
+  void pop_front() {
+    buffer.pop_front();
+    advance(output);
+  }
+
   Token front() const { return buffer.front(); }
+
   tokenizer_t() = delete;
   tokenizer_t(const tokenizer_t&) = delete;
   tokenizer_t& operator=(tokenizer_t&&) = delete;
   tokenizer_t& operator=(const tokenizer_t&) = delete;
+
 private:
-  void advance() {
+
+  template<class O>
+  void advance(O output) {
+
     using namespace rx;
     using namespace std;
+
     string token;
     ignorer ignore;
-    auto output(utf8_output_encoder(output_iterator(back_inserter(token))));
-    while (accept_if(is_whitespace, input, ignore)) {}
+    auto append(utf8_output_encoder(output_iterator(back_inserter(token))));
+
+#   define Accept(X) accept(X, input, append)
+#   define AcceptIf(X) accept_if(X, input, append)
+#   define Ignore(X) accept(X, input, ignore)
+#   define IgnoreIf(X) accept_if(X, input, ignore)
+#   define Many(X) while (X) {}
+#   define Output(X) output.put(X);
+
+    Many(IgnoreIf(is_whitespace))
     if (input.empty()) return;
-    if (accept(U'{', input, ignore)) {
-      buffer.push_back(Token::push());
-    } else if (accept(U'}', input, ignore)) {
-      buffer.push_back(Token::pop());
-    } else if (accept(U'0', input, ignore)) {
-      if (accept(U'b', input, ignore)) {
-        while (accept_if(is_binary, input, output)
-          || accept(U'_', input, ignore)) {}
-        buffer.push_back(write_signed(token, 2));
-      } else if (accept(U'o', input, ignore)) {
-        while (accept_if(is_octal, input, output)
-          || accept(U'_', input, ignore)) {}
-        buffer.push_back(write_signed(token, 8));
-      } else if (accept(U'x', input, ignore)) {
-        while (accept_if(is_hexadecimal, input, output)
-          || accept(U'_', input, ignore)) {}
-        buffer.push_back(write_signed(token, 16));
-      } else if (accept(U'.', input, output)) {
-        while (accept_if(is_decimal, input, output)
-          || accept(U'_', input, ignore)) {}
-        buffer.push_back(write_double(token));
+
+    if (Ignore(U'{')) {
+      Output(Token::push())
+    } else if (Ignore(U'}')) {
+      Output(Token::pop())
+    } else if (Ignore(U'0')) {
+      if (Ignore(U'b')) {
+        Many(AcceptIf(is_binary) || Ignore(U'_'))
+        Output(write_signed(token, 2))
+      } else if (Ignore(U'o')) {
+        Many(AcceptIf(is_octal) || Ignore(U'_'))
+        Output(write_signed(token, 8))
+      } else if (Ignore(U'x')) {
+        Many(AcceptIf(is_hexadecimal) || Ignore(U'_'))
+        Output(write_signed(token, 16))
+      } else if (Accept(U'.')) {
+        Many(AcceptIf(is_decimal) || Ignore(U'_'))
+        Output(write_double(token))
       } else {
-        while (accept_if(is_decimal, input, output)
-          || accept(U'_', input, ignore)) {}
-        buffer.push_back(write_signed(token, 10));
+        Many(AcceptIf(is_decimal) || Ignore(U'_'))
+        Output(write_signed(token, 10))
       }
-    } else if (accept_if(is_decimal, input, output)) {
-      while (accept_if(is_decimal, input, output)
-        || accept(U'_', input, ignore)) {}
-      if (accept(U'.', input, output)) {
-        while (accept_if(is_decimal, input, output)
-          || accept(U'_', input, ignore)) {}
-        buffer.push_back(write_double(token));
+    } else if (AcceptIf(is_decimal)) {
+      Many(AcceptIf(is_decimal) || Ignore(U'_'))
+      if (Accept(U'.')) {
+        Many(AcceptIf(is_decimal) || Ignore(U'_'))
+        Output(write_double(token))
       } else {
-        buffer.push_back(write_signed(token, 10));
+        Output(write_signed(token, 10))
       }
-    } else if (accept_if(is_alphabetic, input, output)) {
-      while (accept_if(is_alphanumeric, input, output)) {}
+    } else if (AcceptIf(is_alphabetic)) {
+      Many(AcceptIf(is_alphanumeric))
       throw runtime_error(join("Unknown keyword: \"", token, "\"."));
     } else {
       throw runtime_error(join("Invalid character '", input.front(), "'."));
     }
+
+#   undef Accept
+#   undef AcceptIf
+#   undef Ignore
+#   undef IgnoreIf
+#   undef Many
+#   undef Output
+
   }
+
   Token write_signed(const std::string& token, int base) {
     char* boundary;
     const auto value = std::strtoll(token.c_str(), &boundary, base);
@@ -84,6 +114,7 @@ private:
         " signed integer literal: \"", token, "\""));
     return Token::write(int64_t(value));
   }
+
   Token write_unsigned(const std::string& token, int base) {
     char* boundary;
     const auto value = std::strtoull(token.c_str(), &boundary, base);
@@ -92,6 +123,7 @@ private:
         " unsigned integer literal: \"", token, "\""));
     return Token::write(uint64_t(value));
   }
+
   Token write_double(const std::string& token) {
     char* boundary;
     const auto value = std::strtod(token.c_str(), &boundary);
@@ -100,8 +132,11 @@ private:
         (join("Invalid floating-point literal: \"", token, "\""));
     return Token::write(value);
   }
+
   R input;
   std::deque<Token> buffer;
+  rx::output_iterator_t<std::back_insert_iterator<decltype(buffer)>> output;
+
 };
 
 template<class R>
