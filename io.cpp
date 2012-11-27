952aa566e33e2ca80e43f77fa65327ae2fb1533b
Jon Purdy <evincarofautumn@gmail.com>
2012-11-26 23:50:02 -0800

    Fix I/O typo.

--- io.cpp
+++ io.cpp
@@ -8,7 +8,7 @@ std::vector<uint32_t> read(std::istream& input) {
   std::vector<char> octets;
   std::vector<uint32_t> result;
   std::istreambuf_iterator<char> begin(input), end;
-  result.assign(begin, end);
+  octets.assign(begin, end);
   utf8::utf8to32(octets.begin(), octets.end(), std::back_inserter(result));
   return result;
 }
